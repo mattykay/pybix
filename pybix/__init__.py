e0cf2616480a9aa6f812d2413654fa6c629043b4
Matt Kalnins <git@matthewkalnins.com>
2019-08-17 21:55:54 +0930

    Removed unused field

--- pybix/__init__.py
+++ pybix/__init__.py
@@ -4,5 +4,4 @@ from .graph import GraphImageAPI
 __version__ = '0.0.2'
 __license__ = "MIT"
 __author__ = "Matthew Kalnins"
-__maintainer__ = "Matthew Kalnins"
 __email__ = "pybix@matthewkalnins.com"
